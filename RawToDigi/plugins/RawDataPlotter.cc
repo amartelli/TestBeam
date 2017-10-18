c47f25c55b8e0ec678b940929033f760f7ef1b34
arabella <arabella.martelli@cern.ch>
2017-10-18 11:34:38 +0200

    to study pedestal vs time sample

--- RawToDigi/plugins/RawDataPlotter.cc
+++ RawToDigi/plugins/RawDataPlotter.cc
@@ -2,6 +2,8 @@
 #include "TH1F.h"
 #include "TH2F.h"
 #include "TH2Poly.h"
+#include "TCanvas.h"
+#include <TStyle.h>
 #include <fstream>
 #include <sstream>
 // user include files
@@ -22,6 +24,20 @@
 #include "HGCal/Geometry/interface/HGCalTBGeometryParameters.h"
 #include <iomanip>
 #include <set>
+
+
+// struct hgcal_channel{
+//   hgcal_channel(): Id(0),
+// 		   iB(0),
+// 		   iS(0),
+// 		   iC(0),
+//                    iAdcHG(0.),
+//                    iAdcLG(0.){;}
+//   int Id;
+//   int IB, iS, iC;
+//   std::vector<std::pair<std::pair<int, int>, float> > iAdcHG;
+//   std::vector<std::pair<std::pair<int, int>, float> > iAdcLG;
+// };
 
 class RawDataPlotter : public edm::one::EDAnalyzer<edm::one::SharedResources>
 {
@@ -45,6 +61,15 @@ private:
   int m_evtID;
   std::map<int,TH1F*> m_h_adcHigh;
   std::map<int,TH1F*> m_h_adcLow;
+  std::map<int,TH1F*> m_h_adcHighDiff;
+  std::map<int,TH1F*> m_h_adcLowDiff;
+  std::map<int, std::map<std::pair<int, int>, std::vector<float> > > m_AdcHG;
+  std::map<int, std::map<std::pair<int, int>, std::vector<float> > > m_AdcLG;
+  std::map<int,TH2F*> m_ToAvsChannel;
+  std::map<int,TH2F*> m_h2_adcHGVsSCA;
+  std::map<int,TH2F*> m_h2_adcLGVsSCA;
+  std::map<int,TH2F*> m_h2_adcHGVsSCAShift;
+  std::map<int,TH2F*> m_h2_adcLGVsSCAShift;
   std::map<int,TH2F*> m_h_pulseHigh;
   std::map<int,TH2F*> m_h_pulseLow;
 
@@ -69,14 +94,27 @@ RawDataPlotter::RawDataPlotter(const edm::ParameterSet& iConfig) :
 
   m_evtID=0;
   
+
+  std::cout << " init iboard tot = " << HGCAL_TB_GEOMETRY::NUMBER_OF_HEXABOARD << std::endl;
+
   std::ostringstream os( std::ostringstream::ate );
   TH2F* htmp2;
+  TH2F* htmp1vsSCA;
   TH1F* htmp1;
+  //  TH1F* htmpValue;
   for(size_t ib = 0; ib<HGCAL_TB_GEOMETRY::NUMBER_OF_HEXABOARD; ib++) {
+    //    if(ib > 0) return;
     for( size_t iski=0; iski<HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA; iski++ ){
       os.str("");os<<"HexaBoard"<<ib<<"_Skiroc"<<iski;
       TFileDirectory dir = fs->mkdir( os.str().c_str() );
-      for( size_t ichan=0; ichan<HGCAL_TB_GEOMETRY::N_CHANNELS_PER_SKIROC; ichan++ ){
+ 
+      os.str("");
+      os << "ToAvsChannel_HexaBoard" << ib << "_Chip" << iski ;
+      htmp2=dir.make<TH2F>(os.str().c_str(),os.str().c_str(),128, 0., 128, 4096, 0., 4096.);
+      m_ToAvsChannel.insert( std::pair<int,TH2F*>(ib*1000+iski*100, htmp2) );
+      std::cout << " inizio ib = " << ib << " ski = " << iski << " vero iski = " << iski << std::endl;
+
+     for( size_t ichan=0; ichan<HGCAL_TB_GEOMETRY::N_CHANNELS_PER_SKIROC; ichan++ ){
 	for( size_t it=0; it<NUMBER_OF_SCA; it++ ){
 	  os.str("");
 	  os << "HighGain_HexaBoard" << ib << "_Chip" << iski << "_Channel" << ichan << "_SCA" << it ;
@@ -86,7 +124,53 @@ RawDataPlotter::RawDataPlotter(const edm::ParameterSet& iConfig) :
 	  os << "LowGain_HexaBoard" << ib << "_Chip" << iski << "_Channel" << ichan << "_SCA" << it ;
 	  htmp1=dir.make<TH1F>(os.str().c_str(),os.str().c_str(),1000,0,4096);
 	  m_h_adcLow.insert( std::pair<int,TH1F*>(ib*100000+iski*10000+ichan*100+it, htmp1) );
+	  
+	  for(int ij=0; ij<13; ++ij){
+	    std::pair<int, int> locPair(it, ij);
+	    std::vector<float> adcV;
+	    adcV.clear();
+	    m_AdcHG[ib*1000+iski*100+ichan].insert(std::pair<std::pair<int, int>, std::vector<float> > (locPair, adcV));
+	    m_AdcLG[ib*1000+iski*100+ichan].insert(std::pair<std::pair<int, int>, std::vector<float> > (locPair, adcV));
+
+	    //if(iski==0 && ichan == 0 && (ib == 2 || ib == 3)){
+	    /*
+	    if(iski==0 && ichan == 0 && ib == 0){
+	      os.str("");
+	      os << "DiffHG_HexaBoard" << ib << "_Chip" << iski << "_Channel" << ichan << "_SCA" << it << "_TS" << ij;
+	      htmpValue=dir.make<TH1F>(os.str().c_str(),os.str().c_str(),1000,0,4096);
+	      m_h_adcHighDiff.insert( std::pair<int,TH1F*>(ib*1000+it*100+ij, htmpValue) );
+
+	      os.str("");
+	      os << "DiffLG_HexaBoard" << ib << "_Chip" << iski << "_Channel" << ichan << "_SCA" << it << "_TS" << ij;
+	      htmpValue=dir.make<TH1F>(os.str().c_str(),os.str().c_str(),1000,0,4096);
+	      m_h_adcLowDiff.insert( std::pair<int,TH1F*>(ib*1000+it*100+ij, htmpValue) );
+	    }
+	    */
+	    
+	  }
 	}
+	os.str("");
+	os << "PedHG_TSvsSCA_HexaBoard" << ib << "_Chip" << iski << "_Channel" << ichan;
+	htmp1vsSCA = dir.make<TH2F>(os.str().c_str(),os.str().c_str(),NUMBER_OF_SCA,0, NUMBER_OF_SCA, NUMBER_OF_SCA,0, NUMBER_OF_SCA);
+	m_h2_adcHGVsSCA[ib*1000+iski*100+ichan] = htmp1vsSCA;
+	os.str("");
+	os << "PedLG_TSvsSCA_HexaBoard" << ib << "_Chip" << iski << "_Channel" << ichan;
+	htmp1vsSCA = dir.make<TH2F>(os.str().c_str(),os.str().c_str(),NUMBER_OF_SCA,0, NUMBER_OF_SCA, NUMBER_OF_SCA,0, NUMBER_OF_SCA);
+	m_h2_adcLGVsSCA[ib*1000+iski*100+ichan] = htmp1vsSCA;
+
+	/*
+	if(ib == 3){
+	os.str("");
+	os << "PedHG_TSvsSCA_HexaBoard_Shift" << ib << "_Chip" << iski << "_Channel" << ichan;
+	htmp1vsSCA = dir.make<TH2F>(os.str().c_str(),os.str().c_str(),NUMBER_OF_SCA,0, NUMBER_OF_SCA, NUMBER_OF_SCA,0, NUMBER_OF_SCA);
+	m_h2_adcHGVsSCAShift[ib*1000+iski*100+ichan] = htmp1vsSCA;
+	os.str("");
+	os << "PedLG_TSvsSCA_HexaBoard_Shift" << ib << "_Chip" << iski << "_Channel" << ichan;
+	htmp1vsSCA = dir.make<TH2F>(os.str().c_str(),os.str().c_str(),NUMBER_OF_SCA,0, NUMBER_OF_SCA, NUMBER_OF_SCA,0, NUMBER_OF_SCA);
+	m_h2_adcLGVsSCAShift[ib*1000+iski*100+ichan] = htmp1vsSCA;
+	}
+	*/
+
 	if( ichan%2==0 ){
 	  os.str("");
 	  os << "HighGainVsSCA_Hexa" << ib << "_Chip" << iski << "_Channel" << ichan;
@@ -97,7 +181,7 @@ RawDataPlotter::RawDataPlotter(const edm::ParameterSet& iConfig) :
 	  htmp2=dir.make<TH2F>(os.str().c_str(),os.str().c_str(),NUMBER_OF_SCA,0, NUMBER_OF_SCA,1000,0,4096);
 	  m_h_pulseLow.insert( std::pair<int,TH2F*>(ib*1000+iski*100+ichan, htmp2) );
 	}
-      }
+     }
     }
   }
 
@@ -142,11 +226,15 @@ void RawDataPlotter::analyze(const edm::Event& event, const edm::EventSetup& set
       }
     }
   }
+
+  //  std::cout << " tot skiroc = " << skirocs->size() << " per board = " << HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA << std::endl;
+
   
   for( size_t iski=0;iski<skirocs->size(); iski++ ){
     HGCalTBSkiroc2CMS skiroc=skirocs->at(iski);
     std::vector<int> rollpositions=skiroc.rollPositions();
     int iboard=iski/HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA;
+    //    if(iboard > 0) return;
     for( size_t ichan=0; ichan<HGCAL_TB_GEOMETRY::N_CHANNELS_PER_SKIROC; ichan++ ){
       HGCalTBDetId detid=skiroc.detid( ichan );
       HGCalTBElectronicsId eid( essource_.emap_.detId2eid(detid.rawId()) );
@@ -158,11 +246,17 @@ void RawDataPlotter::analyze(const edm::Event& event, const edm::EventSetup& set
 	if( rollpositions[it]<9 ){ //rm on track samples+2 last time sample which show weird behaviour
 	  m_h_adcHigh[iboard*100000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*10000+ichan*100+it]->Fill(skiroc.ADCHigh(ichan,it));
 	  m_h_adcLow[iboard*100000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*10000+ichan*100+it]->Fill(skiroc.ADCLow(ichan,it));
+	}
+	//	if(skiroc.TOAFall(ichan) == 4){
+	  std::pair<int, int> locPair(it, rollpositions[it]);
+	  m_AdcHG[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan][locPair].push_back(skiroc.ADCHigh(ichan,it));
+	  m_AdcLG[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan][locPair].push_back(skiroc.ADCLow(ichan,it));
+	  //	}
 	  if( ichan%2==0 ){
 	    m_h_pulseHigh[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Fill( it,skiroc.ADCHigh(ichan,it) ); 
 	    m_h_pulseLow[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Fill( it,skiroc.ADCLow(ichan,it) );
 	  }
-	}
+	
 	if(m_eventPlotter&&essource_.emap_.existsEId(eid) ){
 	  if(!IsCellValid.iu_iv_valid( detid.layer(),detid.sensorIU(), detid.sensorIV(), detid.iu(), detid.iv(), m_sensorsize ) )  continue;
 	  CellCentreXY = TheCell.GetCellCentreCoordinatesForPlots( detid.layer(), detid.sensorIU(), detid.sensorIV(), detid.iu(), detid.iv(), m_sensorsize );
@@ -171,6 +265,9 @@ void RawDataPlotter::analyze(const edm::Event& event, const edm::EventSetup& set
 	  polyMap[ 100*iboard+it ]->Fill(iux/2 , iuy, skiroc.ADCHigh(ichan,it) );
 	}
       }
+      //std::cout << "ib = " << iboard << " ski = " << iski << " ichan = " << ichan << " toa = " << skiroc.TOAFall(ichan) << std::endl;
+      m_ToAvsChannel[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100]->Fill(ichan, skiroc.TOAFall(ichan));
+
     }
   }
 }
@@ -202,6 +299,113 @@ void RawDataPlotter::beginJob()
 
 void RawDataPlotter::endJob()
 {
+  for(size_t iboard = 0; iboard<HGCAL_TB_GEOMETRY::NUMBER_OF_HEXABOARD; iboard++) {
+    //    if(iboard > 0) return;
+    for( size_t iski=0; iski<HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA; iski++ ){
+      for(size_t ichan = 0; ichan<HGCAL_TB_GEOMETRY::N_CHANNELS_PER_SKIROC; ichan++ ){
+	for( size_t it=0; it<NUMBER_OF_SCA; it++ ){
+	  // int sizeLoc = m_AdcHG[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan].size();
+	  // std::cout << " sizeLoc = " << sizeLoc << std::endl;
+	  for(int ij=0; ij<13; ++ij){
+	    std::pair<int, int> locPair(it, ij);
+	    std::vector<float> HGv(m_AdcHG[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan][locPair]);
+
+	    if(HGv.size() == 0){
+	      m_h2_adcHGVsSCA[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Fill(it, ij, -1);
+	      // if(iboard == 3 && ij < 4) m_h2_adcHGVsSCAShift[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Fill(it, ij+9, -1);
+	      // if(iboard == 3 && ij >= 4) m_h2_adcHGVsSCAShift[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Fill(it, ij-4, -1);
+	    }
+	    else{
+	      std::sort(HGv.begin(), HGv.end());
+	      m_h2_adcHGVsSCA[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Fill(it, ij, HGv.at(1.*HGv.size()/2.));
+	      // if(iboard == 3 && ij < 4) m_h2_adcHGVsSCAShift[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Fill(it, ij+9, HGv.at(1.*HGv.size()/2.));
+	      // if(iboard == 3 && ij >= 4) m_h2_adcHGVsSCAShift[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Fill(it, ij-4, HGv.at(1.*HGv.size()/2.));
+	    }
+
+	    std::vector<float> LGv(m_AdcLG[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan][locPair]);
+	    if(LGv.size() == 0){
+	      m_h2_adcLGVsSCA[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Fill(it, ij, -1);
+	      // if(iboard == 3 && ij < 4) m_h2_adcLGVsSCAShift[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Fill(it, ij+9, -1);
+	      // if(iboard == 3 && ij >= 4) m_h2_adcLGVsSCAShift[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Fill(it, ij-4, -1);
+	    }
+	    else{	    
+	      std::sort(LGv.begin(), LGv.end());
+	      m_h2_adcLGVsSCA[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Fill(it, ij, LGv.at(1.*LGv.size()/2.));
+	      // if(iboard == 3 && ij < 4) m_h2_adcLGVsSCAShift[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Fill(it, ij+9, LGv.at(1.*LGv.size()/2.));
+	      // if(iboard == 3 && ij >= 4) m_h2_adcLGVsSCAShift[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Fill(it, ij-4, LGv.at(1.*LGv.size()/2.));
+	    }
+
+	    
+	    //  if(ichan == 0 && iski == 0 && (iboard == 2 || iboard == 3) ){
+	    /*
+	    if(ichan == 0 && iski == 0 && iboard == 0){
+	      //std::cout << " size HG = " << HGv.size() << std::endl;
+	      for(unsigned int jk=0; jk<HGv.size(); ++jk) m_h_adcHighDiff[iboard*1000+it*100+ij]->Fill(HGv.at(jk));
+	      for(unsigned int jk=0; jk<LGv.size(); ++jk) m_h_adcLowDiff[iboard*1000+it*100+ij]->Fill(LGv.at(jk));
+	      //	      std::cout << " post size HG = " << HGv.size() << std::endl;
+	      gStyle->SetOptStat(1);
+	      TCanvas* cHisto = new TCanvas();
+	      cHisto->cd();
+	      m_h_adcHighDiff[iboard*1000+it*100+ij]->GetXaxis()->SetTitle("ADC HG");
+	      m_h_adcHighDiff[iboard*1000+it*100+ij]->Draw();
+	      cHisto->Print(Form("folderRUN/histos/%s.png", m_h_adcHighDiff[iboard*1000+it*100+ij]->GetName() ), ".png");
+	      
+	      TCanvas* cListo = new TCanvas();
+	      cListo->cd();
+	      m_h_adcLowDiff[iboard*1000+it*100+ij]->GetXaxis()->SetTitle("ADC LG");
+	      m_h_adcLowDiff[iboard*1000+it*100+ij]->Draw();
+	      cListo->Print(Form("folderRUN/histos/%s.png", m_h_adcLowDiff[iboard*1000+it*100+ij]->GetName() ), ".png");
+	    }
+	    */
+	    
+	  }
+	}
+	
+	gStyle->SetOptStat(0);
+	
+	TCanvas* cH = new TCanvas();
+	cH->cd();
+	m_h2_adcHGVsSCA[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->GetXaxis()->SetTitle("SCA");
+	m_h2_adcHGVsSCA[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->GetYaxis()->SetTitle("time stamp");
+	m_h2_adcHGVsSCA[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Draw("coltext");
+	cH->Print(Form("folderRUN/%s.png", m_h2_adcHGVsSCA[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->GetName() ), ".png");
+
+	TCanvas* cL = new TCanvas();
+	cL->cd();
+	m_h2_adcLGVsSCA[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->GetXaxis()->SetTitle("SCA");
+	m_h2_adcLGVsSCA[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->GetYaxis()->SetTitle("time stamp");
+	m_h2_adcLGVsSCA[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Draw("coltext");
+	cL->Print(Form("folderRUN/%s.png", m_h2_adcLGVsSCA[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->GetName() ), ".png");
+	/*
+	if(iboard == 3){
+	  TCanvas* cHS = new TCanvas();
+	  cHS->cd();
+	  m_h2_adcHGVsSCAShift[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->GetXaxis()->SetTitle("SCA");
+	  m_h2_adcHGVsSCAShift[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->GetYaxis()->SetTitle("time stamp - 4");
+	  m_h2_adcHGVsSCAShift[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Draw("coltext");
+	  cHS->Print(Form("folderRUN/Shift/%s.png", m_h2_adcHGVsSCAShift[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->GetName() ), ".png");
+	  
+	  TCanvas* cLS = new TCanvas();
+	  cLS->cd();
+	  m_h2_adcLGVsSCAShift[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->GetXaxis()->SetTitle("SCA");
+	  m_h2_adcLGVsSCAShift[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->GetYaxis()->SetTitle("time stamp - 4");
+	  m_h2_adcLGVsSCAShift[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->Draw("coltext");
+	  cLS->Print(Form("folderRUN/Shift/%s.png", m_h2_adcLGVsSCA[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100+ichan]->GetName() ), ".png");
+	}
+	*/
+	
+      }
+      std::cout << " stampo ib = " << iboard << " ski = " << iski << " vero iski = " << iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA << std::endl;
+      TCanvas* cToA = new TCanvas();
+      cToA->cd();
+      gPad->SetLogy();
+      m_ToAvsChannel[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100]->GetXaxis()->SetTitle("channel");
+      m_ToAvsChannel[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100]->GetYaxis()->SetTitle("ToA");
+      m_ToAvsChannel[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100]->Draw("colz");
+      cToA->Print(Form("folderRUN/TOA/%s.png", m_ToAvsChannel[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100]->GetName() ), ".png");
+      //      cToA->Print(Form("folderRUN/TOA/%s.root", m_ToAvsChannel[iboard*1000+(iski%HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA)*100]->GetName() ), ".root");
+    }
+  }
 }
 
 void RawDataPlotter::fillDescriptions(edm::ConfigurationDescriptions& descriptions)
